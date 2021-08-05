0dda92caeb71ed1354023d44d39e19af72277110
Martín Granados García <martin.granados@wizeline.com>
2021-08-04 19:16:58 -0500

    Add TableView Header with labels

---
+++ tlaloc/LandmarkDetailViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LandmarkDetailViewController.swift
+//  tlaloc
+//
+//  Created by Martin Granados Garcia on 7/31/21.
+//
+
+import UIKit
+
+class LandmarkDetailViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+
+        let headerView = UIView.init(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 200))
+        let titleLabel = UILabel()
+        titleLabel.text = "Roma Norte"
+        titleLabel.font = UIFont.systemFont(ofSize: 48)
+        titleLabel.textColor = .white
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let centigrades = UILabel()
+        centigrades.text = "19°C"
+        centigrades.font = UIFont.systemFont(ofSize: 24)
+        centigrades.textColor = .white
+        centigrades.translatesAutoresizingMaskIntoConstraints = false
+
+        let rainDescriptor = UILabel()
+        rainDescriptor.text = "It will rain in 3 hours"
+        rainDescriptor.font = UIFont.systemFont(ofSize: 20)
+        rainDescriptor.textColor = .white
+        rainDescriptor.translatesAutoresizingMaskIntoConstraints = false
+
+        let bigConfiguration = UIImage.SymbolConfiguration(scale: .large)
+        let weatherIcon = UIImage(systemName: "cloud.sun.rain.fill", withConfiguration: bigConfiguration)
+        let iconView = UIImageView(image: weatherIcon)
+        iconView.tintColor = .white
+        iconView.translatesAutoresizingMaskIntoConstraints = false
+        
+        headerView.addSubview(titleLabel)
+        headerView.addSubview(centigrades)
+        headerView.addSubview(rainDescriptor)
+        headerView.addSubview(iconView)
+
+        titleLabel.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 16).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: headerView.leftAnchor, constant: 16).isActive = true
+
+        rainDescriptor.leftAnchor.constraint(equalTo: headerView.leftAnchor, constant: 16).isActive = true
+        rainDescriptor.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16).isActive = true
+
+        NSLayoutConstraint.activate([
+            iconView.topAnchor.constraint(equalTo: rainDescriptor.topAnchor),
+            iconView.leftAnchor.constraint(equalTo: rainDescriptor.rightAnchor, constant: 16),
+            centigrades.topAnchor.constraint(equalTo: rainDescriptor.bottomAnchor, constant: 16),
+            centigrades.leftAnchor.constraint(equalTo: rainDescriptor.leftAnchor)
+        ])
+
+
+        tableView.tableHeaderView = headerView
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        24
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected a cell")
+    }
+}
--- tlaloc/ViewController.swift
+++ tlaloc/ViewController.swift
@@ -16,11 +16,10 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return 20
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CustomCell
-        cell?.backgroundColor = .black
         cell?.titleLabel.text = "Roma Norte"
         cell?.temperatureLabel.text = "19°C"
         return cell ?? UICollectionViewCell()
@@ -28,9 +27,23 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = view.frame.width
-        let height = CGFloat(150)
+        let height = CGFloat(120)
 
         return CGSize(width: width, height: height)
     }
-}
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let landmarkDetail = LandmarkDetailViewController()
+        self.present(landmarkDetail, animated: true, completion: nil)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CustomCell
+        cell?.isSelected = true
+        return true
+    }
+}
